4002f8d21c8df8e2e748cf5ccc718c3f13938394
Mark Farnsworth <farnsworth2008@gmail.com>
2022-11-12 15:23:16 +0000

    0.1.20 Updated `docusaurus`
    - Updated `docusaurus`

--- CHANGES.md
+++ CHANGES.md
@@ -7,5 +7,5 @@ This file describes the project history as it relates to tagged versions.
 ## [0.1.11](.) to [0.1.18](.)
 - General updates
 
-## [0.1.19](.)
+## [0.1.19](.) to [0.1.20](.)
 - Updated `docusaurus`
--- src/pages/index.js
+++ src/pages/index.js
@@ -7,7 +7,7 @@ import HomepageFeatures from '@site/src/components/HomepageFeatures';
 
 import styles from './index.module.css';
 
-const version = '0.1.19-1000';
+const version = '0.1.20';
 
 function HomepageHeader() {
   const {siteConfig} = useDocusaurusContext();
